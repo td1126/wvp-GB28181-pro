f58d8fb0d192b15d979ed69528868307ca819622
tedcxwang <tedcxwang@tencent.com>
2024-11-14 18:10:18 +0800

    新文件：   docker/mysql8/conf/my.cnf 	新文件：   docker/mysql8/start-mysql8.sh 	修改：     src/main/resources/application-dev.yml

---
+++ docker/mysql8/start-mysql8.sh
@@ -0,0 +1,7 @@
+ docker run -d --name mysql8 \
+--privileged=true \
+-v ./conf/my.cnf:/etc/mysql/my.cnf:rw \
+-v ./log:/var/log/mysql \
+-v ./data:/var/lib/mysql \
+-p 3306:3306 -e MYSQL_ROOT_PASSWORD='!MySQL8' \
+mysql
