5f69bd311200419ab1e02536d8b48446b778c964
tedcxwang <tedcxwang@tencent.com>
2024-11-14 18:24:50 +0800

    删除：     docker/mysql8/conf/my.cnf 	修改：     docker/mysql8/start-mysql8.sh

--- docker/mysql8/start-mysql8.sh
+++ docker/mysql8/start-mysql8.sh
@@ -1,6 +1,5 @@
  docker run -d --name mysql8 \
 --privileged=true \
--v ./conf/my.cnf:/etc/mysql/my.cnf:rw \
 -v ./log:/var/log/mysql \
 -v ./data:/var/lib/mysql \
 -p 3306:3306 -e MYSQL_ROOT_PASSWORD='!MySQL8' \
